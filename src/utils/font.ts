ccaa7693af707e78a13e34c9fd60c2d6684e887c
Ritik Srivastava <ritik.space@gmail.com>
2026-02-24 02:08:00 +0530

    Initial dependency setup

---
+++ src/utils/font.ts
@@ -0,0 +1,24 @@
+import { Days_One, Jost, Space_Mono } from "next/font/google";
+
+export const displayFamily = Days_One({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--display-family",
+	display: "swap",
+	preload: true,
+});
+
+export const textFamily = Jost({
+	subsets: ["latin"],
+	variable: "--text-family",
+	display: "swap",
+	preload: true,
+});
+
+export const monoFamily = Space_Mono({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--mono-family",
+	display: "swap",
+	preload: true,
+});
