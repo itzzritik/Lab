bfe3d880fa9548bbd1f06e6ba0a6fa6f21c1ed79
Ritik Srivastava <ritik.space@gmail.com>
2026-05-09 20:23:05 +0530

    upgrade to pnpm 11

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["*.ritik.me"],
+	allowedDevOrigins: ["*.ritik.me"],
 };
 
 export default nextConfig;
